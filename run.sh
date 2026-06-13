401839eff710596b5bc714ebc70ea061d692458b
Twilightzon <91133325+Twilightzon@users.noreply.github.com>
2026-06-13 21:34:22 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-echo '============bail============='
-CUDA_VISIBLE_DEVICES=0 python train.py --dropout 0.5 --hidden 16 --lr 1e-3 --epochs 1000 --model gcn --dataset bail --seed_num 10 --tem 0.5 --gamma 0.1 --lr_w 1 --beta_ib 10
-
-echo '============pokec_n============='
-CUDA_VISIBLE_DEVICES=0 python train.py --dropout 0.5 --hidden 16 --lr 0.0005 --epochs 1000 --model gcn --dataset pokec_n --seed_num 10 --tem 0.9 --gamma 0.001 --lr_w 1 
